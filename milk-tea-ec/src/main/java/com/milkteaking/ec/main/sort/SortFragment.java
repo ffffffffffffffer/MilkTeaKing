5719e58e87a060dfc6bf422c70d2db023afbd290
Tan <a610757257@163.com>
2018-05-18 15:12:17 +0800

    分类页面开发

--- milk-tea-ec/src/main/java/com/milkteaking/ec/main/sort/SortFragment.java
+++ milk-tea-ec/src/main/java/com/milkteaking/ec/main/sort/SortFragment.java
@@ -1,10 +1,13 @@
 package com.milkteaking.ec.main.sort;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.milkteaking.core.fragments.bottom.BottomItemFragment;
 import com.milkteaking.ec.R;
+import com.milkteaking.ec.main.sort.list.VerticalListFragment;
+import com.milkteaking.ec.main.sort.sort.SortContentFragment;
 
 /**
  * @author TanJJ
@@ -20,6 +23,21 @@ public class SortFragment extends BottomItemFragment {
 
     @Override
     public void onBindView(Bundle savedInstanceState, View view) {
+
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+
+        // 懒加载,只希望在点击分类的时候才去加载
+        VerticalListFragment verticalListFragment = new VerticalListFragment();
+        // 添加到delegate_sort布局中的左边的ContentFrameLayout
+        loadRootFragment(R.id.vertical_list_container, verticalListFragment);
+        // 当点击左边列表的数据时切换右边的Fragment
+        // 设置右边Fragment默认显示分类一
+        SortContentFragment sortContentFragment = SortContentFragment.create(1);
+        loadRootFragment(R.id.sort_content_container, sortContentFragment);
 
     }
 }
---
+++ milk-tea-ec/src/main/java/com/milkteaking/ec/main/sort/list/VerticalListFragment.java
@@ -0,0 +1,25 @@
+package com.milkteaking.ec.main.sort.list;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.milkteaking.core.fragments.MilkTeaFragment;
+import com.milkteaking.ec.R;
+
+/**
+ * @author TanJJ
+ * @time 2018/5/18 14:49
+ * @des 垂直列表Fragment
+ */
+
+public class VerticalListFragment extends MilkTeaFragment {
+    @Override
+    public Object getLayout() {
+        return R.layout.fragment_vertical_list;
+    }
+
+    @Override
+    public void onBindView(Bundle savedInstanceState, View view) {
+
+    }
+}
